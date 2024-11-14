d5e29e96ed20b8043a93c071b74e217f532051aa
seunghye <esther8310@gmail.com>
2024-11-14 14:22:33 +0900

    first init - db 및 gradle 설정

---
+++ src/test/kotlin/com/seung/survey_kotlin/SurveyKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.seung.survey_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SurveyKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
